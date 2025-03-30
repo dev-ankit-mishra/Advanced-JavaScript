46b4aee18a81c1793bd12f09c7c72b51db75e167
dev-ankit-mishra <developer.ankitmishra@gmail.com>
2025-03-30 22:55:04 +0530

    Add Two Promises[LeetCode-2723]

---
+++ Questions/AddTwoPromises.js
@@ -0,0 +1,16 @@
+//Problem Link-https://leetcode.com/problems/add-two-promises/submissions/1591280842/?envType=study-plan-v2&envId=30-days-of-javascript
+
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    return await promise1+await promise2
+
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
